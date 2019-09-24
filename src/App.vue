16325224d4bab3c9a6ae64f30009ef9a31f45b38
Vinicius Azevedo <vinicius.azevedo@convenia.com.br>
2019-09-24 20:13:03 -0300

    :construction: :tada: init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,124 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="board">
+    <ul v-for="(groupName, groupIndex) of groups" :key="groupName">
+
+      <header>{{ groupName }}</header>
+      <slot>
+        <!-- :class="{ 'is-hovering': card == hover }" -->
+        <li
+          v-for="card of getCards(groupName)"
+          :key="card.id"
+          :data-id="card.id"
+          :data-order="card.order"
+          draggable="true"
+          @dragstart="e => dragStart(e, card, groupName, groupIndex)"
+          @dragover="e => dragOver(e, card)"
+          @drop="e => dragDrop(e, groupName)"
+        >
+          <main>{{ card.task }}</main>
+        </li>
+      </slot>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import data from './db'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: 'vue-coe-dnd',
+
+  data () {
+    return {
+      data: data,
+      // hover: null,
+      draggingCard: {}
+    }
+  },
+
+  computed: {
+    groups () {
+      console.log('computed')
+      return data.map(({ group }) => group)
+    }
+  },
+
+  methods: {
+    getCards (groupName) {
+      return this.data.find(({ group }) => group === groupName).cards
+    },
+
+    dragStart (e, card, groupName, groupIndex) {
+      this.draggingCard = {
+        ...card,
+        groupIndex,
+        group: groupName
+      }
+    },
+
+    dragOver (e, card) {
+      // this.hover = card
+
+      e.preventDefault()
+    },
+
+    dragDrop (e, groupName) {
+      // this.hover = null
+
+      const { order, id } = e.target.closest('li').dataset
+      console.log({
+        id,
+        order,
+        groupName,
+        draggingCard: this.draggingCard,
+        groupIndex: this.draggingCard.groupIndex
+      })
+
+      // this.data = [ ...this.data ].map(card => card.id === this.draggingCard.id ? { ...card, groupName } : card)
+      // this.data[0].group = groupName
+
+      // remover da lista atual
+      this.data = this.data.reduce((acc, { group, cards }) => {
+        if (group === this.draggingCard.group) {
+          return cards.filter(card => card.id === this.draggingCard.id)
+        }
+
+        return acc
+      }, [])
+      // .filter(card => console.log(card, this.draggingCard.id) || card.id === this.draggingCard.id))
+
+      // console.log(this.data[this.draggingCard.groupIndex].cards.splice(1, 0))
+
+      this.draggingCard = {}
+    }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.board {
+  display: flex;
+  padding: 30px 0;
+  height: 100%;
+
+  & > ul {
+    // display: flex;
+    list-style-type: none;
+
+    height: 100%;
+    padding: 0 15px;
+    flex: 0 0 320px;
+
+    & > li {
+      padding: 30px;
+      margin-top: 20px;
+      margin-right: 20px;
+      border: 1px solid red;
+
+      &.is-hovering {
+        margin-top: 100px;
+      }
+    }
+  }
 }
 </style>
---
+++ src/db.js
@@ -0,0 +1,89 @@
+export default [
+  {
+    group: 'backlog',
+    order: 1,
+    cards: [
+      {
+        id: 1,
+        order: 1,
+        task: 'task 11'
+      },
+      {
+        id: 2,
+        order: 2,
+        task: 'task 12'
+      },
+      {
+        id: 3,
+        order: 3,
+        task: 'task 13'
+      }
+    ]
+  },
+
+  {
+    group: 'todo',
+    order: 2,
+    cards: [
+      {
+        id: 4,
+        order: 1,
+        task: 'task 21'
+      },
+      {
+        id: 5,
+        order: 2,
+        task: 'task 22'
+      },
+      {
+        id: 6,
+        order: 3,
+        task: 'task 23'
+      }
+    ]
+  },
+
+  {
+    group: 'doing',
+    order: 3,
+    cards: [
+      {
+        id: 7,
+        order: 1,
+        task: 'task 31'
+      },
+      {
+        id: 8,
+        order: 2,
+        task: 'task 32'
+      },
+      {
+        id: 9,
+        order: 3,
+        task: 'task 33'
+      }
+    ]
+  },
+
+  {
+    group: 'done',
+    order: 4,
+    cards: [
+      {
+        id: 10,
+        order: 1,
+        task: 'task 41'
+      },
+      {
+        id: 11,
+        order: 2,
+        task: 'task 42'
+      },
+      {
+        id: 12,
+        order: 3,
+        task: 'task 43'
+      }
+    ]
+  }
+]
