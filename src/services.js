3e86946bb0a195b6c86c573d089408e8c2629e75
Vinicius Azevedo <viniazvd@gmail.com>
2019-09-26 02:20:50 -0300

    :truck: move to services

--- src/services.js
+++ src/services.js
@@ -1,9 +1,35 @@
-export function removeDuplicates (arr, prop) {
-  const digest = arr.reduce((acc, el) => {
-    if (!acc.has(el[prop])) acc.set(el[prop], el)
+export function getClosest (e, el) {
+  return e.target.closest(el)
+}
 
-    return acc
-  }, new Map())
+function insert (arr, index, newItem) {
+  return [
+    ...arr.slice(0, index), // part of the array before the specified index
+    newItem,                // inserted item
+    ...arr.slice(index)     // part of the array after the specified index
+  ]
+}
 
-  return [...digest.values()].map(({ group }) => group)
+export function addCard (data, _card, card, targetIndex, targetGroup) {
+  if (_card.group === targetGroup) {
+    return {
+      ...data,
+      cards: insert(data.cards, targetIndex, card)
+    }
+  }
+
+  return _card
+}
+
+export function removeCard (data, draggedCard) {
+  const { id: draggedId, group: draggedGroupName } = draggedCard
+
+  if (data.group === draggedGroupName) {
+    return {
+      ...data,
+      cards: data.cards.filter(card => card.id !== draggedId)
+    }
+  }
+
+  return data
 }
