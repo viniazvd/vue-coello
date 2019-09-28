c0db7a145ac969ae4270ca30059779259508c1fc
Vinicius Azevedo <viniazvd@gmail.com>
2019-09-28 19:32:25 -0300

    :zap: config entry component #Board

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-import App from './App.vue'
+import Board from './Board.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(Board)
 }).$mount('#app')
