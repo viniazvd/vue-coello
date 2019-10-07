3fbca6d3e31856b2437d9d3321452228969f26ee
Vinicius Azevedo <vinicius.azevedo@convenia.com.br>
2019-10-07 19:18:59 -0300

    :zap: labels -> tags / comments -> activitys

--- src/db.js
+++ src/db.js
@@ -6,7 +6,7 @@ export default [
       {
         id: 1,
         order: 1,
-        labels: [
+        tags: [
           { type: 'blocked', color: 'blue' },
           { type: 'backend', color: 'red' },
           { type: 'frontend', color: 'green' }
@@ -27,7 +27,7 @@ export default [
         ],
         title: 'tituluzin boladaum pesadaum que não é um QA',
         description: 'task 11',
-        comments: [
+        activitys: [
           { user: 1, date: '29/01/1989', comment: 'coe' },
           { user: 1, date: '29/01/1989', comment: 'coe' },
           { user: 2, date: '29/01/1989', comment: 'coe' }
@@ -40,51 +40,51 @@ export default [
       {
         id: 2,
         order: 2,
-        labels: [],
+        tags: [],
         members: [],
         title: 'tituluzin 12',
         description: 'task 12',
-        comments: [ { user: 1, date: '', comment: 'coe' } ],
+        activitys: [ { user: 1, date: '', comment: 'coe' } ],
         attachments: []
       },
       {
         id: 3,
         order: 3,
-        labels: [],
+        tags: [],
         members: [],
         title: 'tituluzin 13',
         description: 'task 13',
-        comments: [ { user: 1, date: '', comment: 'coe' } ],
+        activitys: [ { user: 1, date: '', comment: 'coe' } ],
         attachments: []
       },
       {
         id: 4,
         order: 4,
-        labels: [],
+        tags: [],
         members: [],
         title: 'tituluzin 14',
         description: 'task 14',
-        comments: [ { user: 1, date: '', comment: 'coe' } ],
+        activitys: [ { user: 1, date: '', comment: 'coe' } ],
         attachments: []
       },
       {
         id: 5,
         order: 5,
-        labels: [],
+        tags: [],
         members: [],
         title: 'tituluzin 15',
         description: 'task 15',
-        comments: [ { user: 1, date: '', comment: 'coe' } ],
+        activitys: [ { user: 1, date: '', comment: 'coe' } ],
         attachments: []
       },
       {
         id: 6,
         order: 6,
-        labels: [],
+        tags: [],
         members: [],
         title: 'tituluzin 16',
         description: 'task 16',
-        comments: [ { user: 1, date: '', comment: 'coe' } ],
+        activitys: [ { user: 1, date: '', comment: 'coe' } ],
         attachments: []
       }
     ]
@@ -103,31 +103,31 @@ export default [
       {
         id: 7,
         order: 1,
-        labels: [],
+        tags: [],
         members: [],
         title: 'tituluzin 31',
         description: 'task 31',
-        comments: [ { user: 1, date: '', comment: 'coe' } ],
+        activitys: [ { user: 1, date: '', comment: 'coe' } ],
         attachments: []
       },
       {
         id: 8,
         order: 2,
-        labels: [],
+        tags: [],
         members: [],
         title: 'tituluzin 32',
         description: 'task 32',
-        comments: [ { user: 1, date: '', comment: 'coe' } ],
+        activitys: [ { user: 1, date: '', comment: 'coe' } ],
         attachments: []
       },
       {
         id: 9,
         order: 3,
-        labels: [],
+        tags: [],
         members: [],
         title: 'tituluzin 33',
         description: 'task 33',
-        comments: [ { user: 1, date: '', comment: 'coe' } ],
+        activitys: [ { user: 1, date: '', comment: 'coe' } ],
         attachments: []
       }
     ]
@@ -140,31 +140,31 @@ export default [
       {
         id: 10,
         order: 1,
-        labels: [],
+        tags: [],
         members: [],
         title: 'tituluzin 41',
         description: 'task 41',
-        comments: [ { user: 1, date: '', comment: 'coe' } ],
+        activitys: [ { user: 1, date: '', comment: 'coe' } ],
         attachments: []
       },
       {
         id: 11,
         order: 2,
-        labels: [],
+        tags: [],
         members: [],
         title: 'tituluzin 42',
         description: 'task 42',
-        comments: [ { user: 1, date: '', comment: 'coe' } ],
+        activitys: [ { user: 1, date: '', comment: 'coe' } ],
         attachments: []
       },
       {
         id: 12,
         order: 3,
-        labels: [],
+        tags: [],
         members: [],
         title: 'tituluzin 43',
         description: 'task 43',
-        comments: [ { user: 1, date: '', comment: 'coe' } ],
+        activitys: [ { user: 1, date: '', comment: 'coe' } ],
         attachments: []
       }
     ]
